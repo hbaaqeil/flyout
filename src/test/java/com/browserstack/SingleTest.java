8b51a39d53956aeaf0e750a2b247ec87e343b004
hbaaqeil <66650114+hbaaqeil@users.noreply.github.com>
2022-06-23 23:50:12 +0100

    Update SingleTest.java

--- src/test/java/com/browserstack/SingleTest.java
+++ src/test/java/com/browserstack/SingleTest.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebElement;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
+import org.testng.selenium.OutputType;
 public class SingleTest extends BrowserStackTestNGTest {
 
     @Test(description = "Sending booking data and submitting the form! Test")
@@ -20,8 +20,10 @@ public class SingleTest extends BrowserStackTestNGTest {
         element3.sendKeys("Hassan");
         WebElement element4 = driver.findElement(By.name("adult1-l"));
         element4.sendKeys("Baaqeil");
+        Thread.sleep(2000);
+        ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
         element1.submit();
-        Thread.sleep(5000);
+        Thread.sleep(2000);
         String titleMsg = driver.getTitle().substring(0,22);
         Assert.assertEquals("Form Successfully Sent", titleMsg);
     }
