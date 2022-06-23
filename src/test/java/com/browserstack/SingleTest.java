cc6e40488cfae6631b2858941114e506923c1da5
hbaaqeil <66650114+hbaaqeil@users.noreply.github.com>
2022-06-23 23:58:02 +0100

    Update SingleTest.java

--- src/test/java/com/browserstack/SingleTest.java
+++ src/test/java/com/browserstack/SingleTest.java
@@ -21,6 +21,8 @@ public class SingleTest extends BrowserStackTestNGTest {
         WebElement element4 = driver.findElement(By.name("adult1-l"));
         element4.sendKeys("Baaqeil");
         Thread.sleep(2000);
+        WebDriver driver = new RemoteWebDriver(new URL(URL), caps);
+        driver = (RemoteWebDriver) new Augmenter().augment(driver);
         ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
         element1.submit();
         Thread.sleep(2000);
