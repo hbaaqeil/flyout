db7da4fd1b97aad988ee078a00e691513fa13041
hbaaqeil <66650114+hbaaqeil@users.noreply.github.com>
2022-06-23 22:57:01 +0100

    Update SingleTest.java

--- src/test/java/com/browserstack/SingleTest.java
+++ src/test/java/com/browserstack/SingleTest.java
@@ -14,7 +14,7 @@ public class SingleTest extends BrowserStackTestNGTest {
         WebElement element1 = driver.findElement(By.id("mobile"));
         element1.sendKeys("0565133578");
         WebElement element2 = driver.findElement(By.id("email"));
-        element2.clear()
+        element2.clear();
         element2.sendKeys("ahbaaqeil@gmail.com");
         WebElement element3 = driver.findElement(By.id("adult1-f"));
         element3.sendKeys("Hassan");
