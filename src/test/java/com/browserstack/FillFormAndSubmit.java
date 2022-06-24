682e6cae0ac1d4cef8eb0a93562c23d0fdea8f49
hbaaqeil <66650114+hbaaqeil@users.noreply.github.com>
2022-06-24 04:45:20 +0100

    Update FillFormAndSubmit.java

--- src/test/java/com/browserstack/FillFormAndSubmit.java
+++ src/test/java/com/browserstack/FillFormAndSubmit.java
@@ -33,7 +33,7 @@ public class FillFormAndSubmit extends BrowserStackTestNGTest {
 
     @Test(description = "Sending booking data and submitting the form! Assert2")
     public void test2() throws Exception {
-       String nameMsg = driver.findElement(By.id("adult1-f"));
+       String nameMsg = driver.findElement(By.id("adult1-f")).value;
         System.out.println("found this Name:" + nameMsg );
         Assert.assertEquals("Hassan", nameMsg);
     }   
