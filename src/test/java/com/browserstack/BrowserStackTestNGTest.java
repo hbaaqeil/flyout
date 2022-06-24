ad088df9ffb8a17ee353b9742db46914dacaf99e
hbaaqeil <66650114+hbaaqeil@users.noreply.github.com>
2022-06-24 05:03:15 +0100

    Update BrowserStackTestNGTest.java

--- src/test/java/com/browserstack/BrowserStackTestNGTest.java
+++ src/test/java/com/browserstack/BrowserStackTestNGTest.java
@@ -69,7 +69,8 @@ public class BrowserStackTestNGTest {
         
         String buildName  = System.getenv("BROWSERSTACK_BUILD_NAME");
         if(buildName == null) {
-            buildName = (String) config.get("buildName");
+            buildName = (String) "jenkins-FlyOutTestNG-51";
+            build = (String) "jenkins-FlyOutTestNG-51";
         }
 
         if(capabilities.getCapability("browserstack.local") != null && capabilities.getCapability("browserstack.local") == "true"){
